a70e879cf2ac57ba34907e5e2f9885e7e41952fc
Husayn / Fionn <124906203+HusaynPrograms@users.noreply.github.com>
2025-02-26 13:52:49 +0000

    Update app.component.ts

--- Desktop/Lab 4/data-binding-app/src/app/app.component.ts
+++ Desktop/Lab 4/data-binding-app/src/app/app.component.ts
@@ -6,20 +6,22 @@ import { ListComponent  } from './list/list.component';
 
 @Component({
   selector: 'app-root',
+  // Importing external component
   imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  count: number = 0;
-  title = 'data-binding-app';
-  show:boolean = true;
+  count: number = 0; // Track Button Clicks
+  title = 'data-binding-app'; // App title
+  show:boolean = true; 
 
-
+//Increase count when button is clicked
   onButtonClick(){
     this.count++
   }
 
+  // Change star visibility when double clicked
   onStarDoubleClicked(){
     if (this.show)
  {
